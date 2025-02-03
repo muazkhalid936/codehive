0d62d4de06a4627bba5c08dcd49278ae54c0ee9b
Muaz Khalid <itzmajo786@gmail.com>
2025-02-03 16:31:28 +0500

    Add config for mp4 files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack(config) {
+        config.module.rules.push({
+          test: /\.(mp4|webm|ogg|swf|ogv)$/, // Video file formats
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                publicPath: '/_next/static/videos/', // Path where videos will be served
+                outputPath: 'static/videos/', // Path where videos will be stored
+                name: '[name].[hash].[ext]', // Naming convention for the files
+                esModule: false,
+              },
+            },
+          ],
+        });
+    
+        return config;
+},
+};
 
 export default nextConfig;
